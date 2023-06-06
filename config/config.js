b827938209c6254ac2f8bd9e16b6978edafbdf9e
Christopher <christopherperez@Christophers-Laptop.local>
2023-06-06 16:51:26 -0500

    server

--- config/config.js
+++ config/config.js
@@ -19,7 +19,7 @@ const sequelize = process.env.JAWSDB_URL
         {
             host: 'localhost',
             dialect: 'mysql',
-            port: 3001
+            port: 3306
         });
 
 module.exports = sequelize;
